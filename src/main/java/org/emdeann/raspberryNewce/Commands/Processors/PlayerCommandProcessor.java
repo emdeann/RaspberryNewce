653f51558d8f96149ebd694103c60be324bac115
Dean <69436131+emdeann@users.noreply.github.com>
2025-07-20 14:36:17 -0700

    Use player param in PlayerCommandProcessor

--- src/main/java/org/emdeann/raspberryNewce/Commands/Processors/PlayerCommandProcessor.java
+++ src/main/java/org/emdeann/raspberryNewce/Commands/Processors/PlayerCommandProcessor.java
@@ -1,18 +1,34 @@
 package org.emdeann.raspberryNewce.Commands.Processors;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.emdeann.raspberryNewce.Commands.CommandParameterTypes;
 import org.emdeann.raspberryNewce.Commands.GameCommand;
 import org.emdeann.raspberryNewce.RaspberryNewcePlugin;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PlayerCommandProcessor extends CommandProcessor {
+
 
     public PlayerCommandProcessor(RaspberryNewcePlugin plugin) {
         super(plugin);
     }
 
-    @GameCommand(paramList = {CommandParameterTypes.INTEGER, CommandParameterTypes.INTEGER, CommandParameterTypes.INTEGER})
-    public void setTilePos(int x, int y, int z) {
-        world.getPlayers().getFirst().teleport(new Location(world, x, y, z));
+    @GameCommand(paramList = {
+            CommandParameterTypes.PLAYER,
+            CommandParameterTypes.INTEGER,
+            CommandParameterTypes.INTEGER,
+            CommandParameterTypes.INTEGER
+    })
+    public void setTilePos(Player player, int x, int y, int z) {
+        player.teleport(new Location(player.getWorld(), x, y, z));
+    }
+
+    @GameCommand(paramList = {CommandParameterTypes.PLAYER})
+    public List<Integer> getTilePos(Player player) {
+        Location loc = player.getLocation();
+        return Arrays.asList(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
     }
 }
